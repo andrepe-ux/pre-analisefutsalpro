fa1e68da5aa12b42679379f7293edb09102f1492
andrepe-ux <amanuelmpereira@gmail.com>
2026-09-07 21:47:03 +0100

    Update user credentials in users.js

--- users.js
+++ users.js
@@ -1,9 +1,10 @@
 // users.js - Gestão de Utilizadores autorizados
 const globalUsersList = [
-    { username: "andre", password: "123" },
+    { username: "AndrePereira", password: "Andre13__" },
     { username: "Dinamo", password: "Sub19" },
     { username: "Martim", password: "martim27" },
-    { username: "AnaPereira", password: "Ana13__" }
+    { username: "AnaPereira", password: "Ana13__" },
+    { username: "Dinamo", password: "Sub15" }
     // Podes adicionar mais utilizadores aqui conforme precisares:
     // { username: "novo_utilizador", password: "sua_password" }
 ];
