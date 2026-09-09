cf8abb1d76c31a8b5c1031f6c92a616d68ef78aa
andrepe-ux <amanuelmpereira@gmail.com>
2026-09-09 09:43:47 +0100

    Add new players to the Sub 15 team

--- plantels.js
+++ plantels.js
@@ -2,8 +2,23 @@ const globalPlantelsData = {
     "escaloes": {
         "Sub 15": {
             "Dinamo": [
-                { "number": 27, "name": "Martim Pereira" },
-                { "number": 14, "name": "Bernardo Vasconcelos" }
+                { "number": 1, "name": "Afonso Peixoto" },
+                { "number": 12, "name": "Gonçalo Marques" },
+                { "number": 2, "name": "Rodrigo Almeida" },
+                { "number": 3, "name": "João Pinho" },
+                { "number": 4, "name": "Henrique Correia" },
+                { "number": 5, "name": "Samuel Santos" },
+                { "number": 6, "name": "Gonçalo Silva" },
+                { "number": 7, "name": "Miguel Costa" },
+                { "number": 8, "name": "Gabriel Rocha" },
+                { "number": 9, "name": "Rubén Costa" },    
+                { "number": 10, "name": "Martim Barge" },
+                { "number": 11, "name": "Gabriel Silva" },
+                { "number": 12, "name": "Rodrigo Vieira" },
+                { "number": 13, "name": "Leandro Silva" },
+                { "number": 14, "name": "Dinis Queijinho" },
+                { "number": 15, "name": "Davi Simões" },
+                { "number": 16, "name": "André Tavares" }            
                 // Podes adicionar mais atletas aqui à vontade:
                 // { "number": 10, "name": "Nome do Atleta" }
             ]
