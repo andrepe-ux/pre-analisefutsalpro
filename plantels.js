d233660b9609fe33582cfe25033110fd48465b35
andrepe-ux <amanuelmpereira@gmail.com>
2026-09-12 01:00:14 +0100

    Update Sub 19 Dinamo team roster
    Added new players to the Sub 19 Dinamo team roster.

--- plantels.js
+++ plantels.js
@@ -25,11 +25,26 @@ const globalPlantelsData = {
         },
         "Sub 19": {
             "Dinamo": [
-                { "number": 27, "name": "Martim Pereira" },
-                { "number": 14, "name": "Bernardo Vasconcelos" },
-                { "number": 17, "name": "Tiago Marcelino" },
-                { "number": 1, "name": "João Matos" },
-                { "number": 2, "name": "Vasco Lino" }
+                { "number": 1, "name": "Gui" },
+                { "number": 14, "name": "Becas" },
+                { "number": 27, "name": "Martim" },
+                { "number": 2, "name": "Veloso" },
+                { "number": 3, "name": "Leo" },
+                { "number": 4, "name": "Tono" },
+                { "number": 5, "name": "Santos" },
+                { "number": 6, "name": "Valente" },
+                { "number": 7, "name": "Dudu" },
+                { "number": 8, "name": "Manú" },
+                { "number": 9, "name": "Chico" },
+                { "number": 10, "name": "Vasco" },
+                { "number": 11, "name": "Gui " },
+                { "number": 13, "name": "Renato" },
+                { "number": 17, "name": "Marcelino" },
+                { "number": 20, "name": "Tiago Santos" },
+                { "number": 47, "name": "Sousa" },
+                { "number": 91, "name": "Matos" },
+                { "number": 92, "name": "João Silva" },
+                { "number": 93, "name": "Rodri" }
             ]
         }
     }
